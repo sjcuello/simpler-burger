171b8dd3d34b3bc9183b0ac4175a300bfc502754
Santiago Cuello <sjcuello@gmail.com>
2021-09-30 01:32:12 -0300

    Selectables are created

--- src/components/Group/index.tsx
+++ src/components/Group/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import useFetchFakeApi from "../../hooks/useFetchFakeApi";
 import { Item } from "../../interfaces/Item";
-import { ComboGroup, ComboSection, SizeOrder, SodasOrder, TitleSection, ToppingsSection } from "./styles";
+import { ComboGroup, ComboSection, Divider, SizeOrder, SodasOrder, TitleSection, ToppingsSection, Form, ItemSection, RadioButton, Checkbox } from "./styles";
 
 interface Props {
     colorfill?: boolean,
@@ -14,52 +14,94 @@ const Group: React.FC<Props> = ({ }) => {
 
     const toppings: Item[] = useFetchFakeApi([], `${API}toppings`)
 
-    const flavors: Item[] = useFetchFakeApi([], `${API}flavors`)
+    const flavours: Item[] = useFetchFakeApi([], `${API}flavours`)
 
     const sizes: Item[] = useFetchFakeApi([], `${API}sizes`)
 
 
-    const [selected, setSelected] = useState('option1')
+    const [sizeSelected, setSizeSelected] = useState(null)
+    const [sodaSelected, setSodaSelected] = useState(null)
+    const [toppingSelected, setToppingSelected] = useState([])
 
-    const handleOptionChange = (changeEvent: any) => {
-        setSelected(changeEvent.target.value)
+    const handleOptionChangeSize = (changeEvent: any) => {
+        setSizeSelected(changeEvent.target.value)
+        console.log('Soda selected: ', sizeSelected);
     }
 
-    const handleFormSubmit = (event: any) => {
-        event.preventDefault()
-        console.log('You have selected:', selected);
+    const handleOptionChangeFlavours = (changeEvent: any) => {
+        setSodaSelected(changeEvent.target.value)
+        console.log('Flavour selected: ', sodaSelected);
     }
 
+    const selectedCheckboxes = new Set<Item>();
+
+    const toggleCheckbox = (topping: Item) => {
+        if (selectedCheckboxes.has(topping)) {
+            selectedCheckboxes.delete(topping);
+        } else {
+            selectedCheckboxes.add(topping);
+        }
+        console.log(`selectedCheckboxes`, Array.from(selectedCheckboxes))
+
+    }
 
     return (
         <ComboGroup>
             <ComboSection>
                 <SizeOrder>
-                    <TitleSection>Titulo</TitleSection>
-                    <form onSubmit={handleFormSubmit}>
+                    <TitleSection>Size</TitleSection>
+                    <Divider></Divider>
+                    <Form>
                         {
                             sizes.length > 0 &&
                             sizes.map((size: Item) => {
-                                console.log(`sizes`, sizes)
                                 return (
-                                    <div key={size.id} className="radio">
-                                        <label>
-                                            <input type="radio" value={size.title} checked={selected === size.title} onChange={handleOptionChange} />
-                                            {size.title}
-                                        </label>
-                                    </div>
+                                    <ItemSection key={size.id}>
+                                        <RadioButton type="radio" value={size.title} checked={sizeSelected === size.title} onChange={handleOptionChangeSize} />
+                                        {size.title}
+                                    </ItemSection>
                                 )
                             })
                         }
-                        <button className="btn btn-default" type="submit">Save</button>
-                    </form>
+                    </Form>
                 </SizeOrder>
                 <SodasOrder>
-
+                    <TitleSection>Sodas flavours</TitleSection>
+                    <Divider></Divider>
+                    <Form column={true}>
+                        {
+                            flavours.length > 0 &&
+                            flavours.map((flavour: Item) => {
+                                return (
+                                    <ItemSection key={flavour.id}>
+                                        <RadioButton type="radio" value={flavour.title} checked={sodaSelected === flavour.title} onChange={handleOptionChangeFlavours} />
+                                        {flavour.title}
+                                    </ItemSection>
+                                )
+                            })
+                        }
+                    </Form>
                 </SodasOrder>
             </ComboSection>
             <ToppingsSection>
+                <TitleSection>Toppings</TitleSection>
+                <Divider></Divider>
+                <Form column={true}>
+                    {
+                        toppings.length > 0 &&
+                        toppings.map((topping: Item) => {
+                            return (
+                                <ItemSection key={topping.id}>
+                                    <Checkbox
+                                        onChange={() => toggleCheckbox(topping)}
+                                    />
+                                    {topping.title}
+                                </ItemSection>
+                            )
+                        })
+                    }
 
+                </Form>
             </ToppingsSection>
         </ComboGroup>
 
--- src/components/Group/styles.ts
+++ src/components/Group/styles.ts
@@ -16,26 +16,23 @@ export const ComboSection = styled.div`
     width: 100%;
     height: 100%;
 `
-
-export const ToppingsSection = styled.div`
-    background-color: red;
-    border-radius: 0 0 .5rem .5rem;
+export const Divider = styled.div`
+    align-self: center;
     width: 100%;
-    height: 100%;
-`
-export const SizeOrder = styled.div`
-    background-color: green;
-    border-radius: .5rem 0 0 0;
-    width:100%;
+    border: 1px solid rgba(0, 0, 0, 0.12);
 `
 
-export const SodasOrder = styled.div`
-    background-color: blue;
-    border-radius: 0 .5rem 0 0;
-    width:100%;
+interface FormProps  {
+    column?: boolean
+}
+export const Form = styled.form`
+    display: grid;
+    grid-auto-rows: 2rem;
+    grid-template-columns: ${(props: FormProps)=>(props.column ? "repeat(auto-fit, minmax(3rem, 1fr))" : "1fr")};
+    align-items: start;
 `
-
 export const TitleSection = styled.h1`
+    display: flex;
     font-style: normal;
     font-weight: 500;
     font-size: 1rem;
@@ -44,9 +41,60 @@ export const TitleSection = styled.h1`
 `
 
 export const ItemSection = styled.p`
+    display: flex;
+    justify-content: flex-start;
+    align-items: end;
+    margin: .5rem 0;
     font-style: normal;
     font-weight: normal;
     font-size: .75rem;
     line-height: .75rem;
+    width: 100%;
     color: #000000;
+`
+
+export const ToppingsSection = styled.div`
+    border-radius: 0 0 .5rem .5rem;
+    width: 100%;
+    height: 100%;
+    padding: 1rem;
+    -webkit-box-sizing: border-box; 
+    -moz-box-sizing: border-box;    
+    box-sizing: border-box; 
+`
+
+export const SizeOrder = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: top;
+    align-items: flex-start;
+    border-radius: .5rem 0 0 0;
+    width:100%;
+    padding: 1rem;
+    -webkit-box-sizing: border-box; 
+    -moz-box-sizing: border-box;    
+    box-sizing: border-box; 
+`
+
+export const SodasOrder = styled.div`
+    border-radius: 0 .5rem 0 0;
+    width:100%;
+    padding: 1rem;
+    -webkit-box-sizing: border-box; 
+    -moz-box-sizing: border-box;    
+    box-sizing: border-box; 
+`
+
+
+export const RadioButton = styled.input.attrs({
+    type: "radio",
+})`
+    cursor: pointer;
+    filter: hue-rotate(159deg);
+`
+export const Checkbox = styled.input.attrs({
+    type: "checkbox",
+})`
+    cursor: pointer;
+    filter: hue-rotate(159deg);
 `
