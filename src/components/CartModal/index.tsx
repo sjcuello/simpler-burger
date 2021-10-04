bcc3782212cb474d32899ef9e65ae0a8bd2151fd
Santiago Cuello <sjcuello@gmail.com>
2021-10-04 04:16:36 -0300

    Delete order

--- src/components/CartModal/index.tsx
+++ src/components/CartModal/index.tsx
@@ -1,4 +1,4 @@
-import { useRecoilValue } from "recoil"
+import { useRecoilState } from "recoil"
 import { cart } from "../../recoil/atoms"
 import { Cart, Sign, Title, SubTitle } from "../globalStyles"
 import Group from "../Group"
@@ -13,10 +13,10 @@ interface Props {
 
 const CartModal: React.FC<Props> = ({ active, toggle }) => {
 
-    const cartState = useRecoilValue(cart)
+    const [cartState, setCartState] = useRecoilState(cart)
 
-    const changeState = () => {
-        toggle()
+    const deleteItem = (index: number) => {
+        setCartState([...cartState.slice(0, index), ...cartState.slice(index + 1)])
     }
 
     return (
@@ -40,16 +40,16 @@ const CartModal: React.FC<Props> = ({ active, toggle }) => {
                                 <Buttons>
                                     <ButtonCart widthButton="4rem">
                                         <Sign>{item.product.price.currency}</Sign>
-                                        {item.product.price.amount.toFixed(2)}
+                                        {item.total.toFixed(2)}
                                     </ButtonCart>
                                     <ButtonCart
                                         enabled={true}
                                         backgroundColor="#F01C4F"
                                         widthButton="5.4rem"
-                                        onClick={() => changeState()}
+                                        onClick={() => deleteItem(index)}
                                     >
                                         <Cart />
-                                        Select
+                                        Remove
                                     </ButtonCart>
                                 </Buttons>
                                 <GroupContainer>
